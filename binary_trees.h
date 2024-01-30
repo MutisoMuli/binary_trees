4e03ec6ca8d61956f948557c1272261ad5a10574
MutisoMuli <mulimutiso525@gmail.com>
2024-01-31 02:11:59 +0300

    Done

--- binary_trees.h
+++ binary_trees.h
@@ -51,7 +51,8 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 /* Prototypes for 5-binary_tree_is_root.c */
 int binary_tree_is_root(const binary_tree_t *node);
 
-
+/* Prototypes for -binary_tree_preorder.c */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 
 
