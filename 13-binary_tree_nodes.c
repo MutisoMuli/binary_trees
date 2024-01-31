5dedf5fe0340abf711208e4ef5ed56f67316da00
MutisoMuli <mulimutiso525@gmail.com>
2024-01-31 03:11:20 +0300

    Done

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
  * @tree: Tree to count the nodes from.
- * 
+ *
  * Return: Number of nodes counted, or 0 if tree is NULL.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
